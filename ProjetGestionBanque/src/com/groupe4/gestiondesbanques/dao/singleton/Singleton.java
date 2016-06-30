ee6328d7b7940aae3188e6f3a69b1a6cdedc2f07
inti0299 <inti0299@DESKTOP-NIFRS8F>
2016-06-30 12:48:50 +0200

    Singleton

--- ProjetGestionBanque/src/com/groupe4/gestiondesbanques/dao/singleton/Singleton.java
+++ ProjetGestionBanque/src/com/groupe4/gestiondesbanques/dao/singleton/Singleton.java
@@ -1,6 +1,27 @@
 package com.groupe4.gestiondesbanques.dao.singleton;
 
+import org.apache.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+
 public class Singleton {
+	
+	private static SessionFactory sessionfactory;
+	static Logger log = Logger.getLogger("Singleton");
+	
+	static{
+		sessionfactory = new Configuration().configure().buildSessionFactory();
+		log.info("<***************connection :) ************>"); 
+	}
+	private Singleton() {
+		
+	}
+	public static SessionFactory getSessfionfactory() {
+		return sessionfactory;
+	}
+
 	
 	
 
